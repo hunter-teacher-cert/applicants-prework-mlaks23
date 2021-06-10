ab37e0123f14bbe3e435276260fbe1950dcdd4cb
mlaks23 <mlaks@schools.nyc.gov>
2021-06-10 11:01:08 -0400

    I fixed a mistake

--- pre01/Hello.java
+++ pre01/Hello.java
@@ -2,7 +2,7 @@ import java.io.*;
 import java.util.*;
 
 public class Hello{
-  public static void main(String[] arga){
+  public static void main(String[] args){
     System.out.println("Hello world!");
   }
 }
